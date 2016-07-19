0fd2600fe7c6d1e40f02a2e9f92948d5f8c271fa
Vadim Sokoltsov <vforvad@gmail.com>
2016-07-20 00:43:02 +0300

    add menu button and course refreshin

--- Estudy/CoursesViewController.swift
+++ Estudy/CoursesViewController.swift
@@ -12,6 +12,9 @@ let courseCellIdentifier = "courseCell"
 
 class CoursesViewController: ApplicationViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet var tableView: UITableView!
+    @IBOutlet var sidebarButton: UIBarButtonItem!
+    var pageNumber = 1
+    var refreshControl: UIRefreshControl!
     
     var serverResponse:[Course] = []
     
@@ -20,23 +23,59 @@ class CoursesViewController: ApplicationViewController, UITableViewDataSource, U
         tableView.registerNib(UINib(nibName: "CourseViewCell", bundle: nil), forCellReuseIdentifier: "courseCell")
         tableView.estimatedRowHeight = 1300
         tableView.rowHeight = UITableViewAutomaticDimension
-//        self.automaticallyAdjustsScrollViewInsets = false
+        setSidebarButton()
+        setUIForView()
+        setupUIRefreshController()
         loadCoursesList()
+    }
+    
+    //MARK: Set navigation for viewController
+    func setSidebarButton() {
+        if self.revealViewController() != nil {
+            sidebarButton.target = self.revealViewController()
+            sidebarButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+    
+    //MARK: UI Setup
+    func setUIForView() {
+        self.navigationController?.navigationBar.barTintColor = Constants.Colors.mainNavigationItemColor
+        self.tableView.backgroundColor = Constants.Colors.mainBackgroundColor
+        self.tableView.separatorStyle = UITableViewCellSeparatorStyle.None
+    }
+    
+    func setupUIRefreshController() {
+        refreshControl = UIRefreshControl()
+        refreshControl.attributedTitle = NSAttributedString(string: NSLocalizedString("pull_to_refresh", comment: ""))
+        refreshControl.addTarget(self, action: #selector(CoursesViewController.loadCoursesList), forControlEvents: UIControlEvents.ValueChanged)
+        tableView.addSubview(refreshControl)
+    }
+    
+    func setupInfinteScrolling() {
+        self.tableView.infiniteScrollIndicatorStyle = .White
+        self.tableView.addInfiniteScrollWithHandler { (scrollView) -> Void in
+            let tableView = scrollView as! UITableView
+            self.pageNumber += 1
+//            self.loadMoreUsersList()
+            tableView.finishInfiniteScroll()
+        }
     }
     
     //MARK: API requests
     
     func loadCoursesList() {
         CourseFactory.getCollection([:], success: successCoursesLoading, error: failedCourseLoading)
-        }
+    }
     
     func successCoursesLoading(courses: [Course]) {
+        self.refreshControl.endRefreshing()
         serverResponse = courses
         tableView.reloadData()
     }
     
     func failedCourseLoading(error: ServerError!) {
-    
+        self.refreshControl.endRefreshing()
     }
     
     //MARK: UITableViewDelegate and UITableViewDataSource methods
